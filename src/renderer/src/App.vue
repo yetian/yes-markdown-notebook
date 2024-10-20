9315535edd106d2f20dbe85ea7b34e9f75449007
Ye Tian <solaris.navi@gmail.com>
2024-10-20 19:40:23 +0200

    added all dependencies

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,24 +1,21 @@
-<script setup>
-import Versions from './components/Versions.vue'
+<template>
+  <div>
+    <div>top bar</div>
+    <div>
+      <div>project</div>
+      <div>editor</div>
+      <div>previewer</div>
+    </div>
+    <div>footer</div>
+  </div>
+</template>
 
-const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+<script>
+export default {
+  name: 'YesMarkdownNotebook'
+}
 </script>
 
-<template>
-  <img alt="logo" class="logo" src="./assets/electron.svg" />
-  <div class="creator">Powered by electron-vite</div>
-  <div class="text">
-    Build an Electron app with
-    <span class="vue">Vue</span>
-  </div>
-  <p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
-  <div class="actions">
-    <div class="action">
-      <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a>
-    </div>
-    <div class="action">
-      <a target="_blank" rel="noreferrer" @click="ipcHandle">Send IPC</a>
-    </div>
-  </div>
-  <Versions />
-</template>
+<style scoped>
+
+</style>
